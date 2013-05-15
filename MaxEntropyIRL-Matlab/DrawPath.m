0083907981d2b36166a9e790a082c10d5c06f92e
Inge Becht <inge.becht91@gmail.com>
2013-05-15 12:43:48 +0200

    data of level is now loaded in correctly

--- MaxEntropyIRL-Matlab/DrawPath.m
+++ MaxEntropyIRL-Matlab/DrawPath.m
@@ -6,7 +6,9 @@ function DrawPath( path )
         x = path(t,2);
         y = path(t,1);
         f = fill([x-1 x x x-1],[y-1 y-1 y y],'r');
-        alpha(f,t/(2*length(path))+0.3);
+        set(f,'facealpha',t/(2*length(path))+0.3 );
+%
+
     end
     drawnow;
 end
--- MaxEntropyIRL-Matlab/LoadMap.m
+++ MaxEntropyIRL-Matlab/LoadMap.m
@@ -1,4 +1,4 @@
-function [map, obst, gates, items] = LoadMap(mapfile)
+function [map, obst] = LoadMap(mapfile)
 %LOADMAP loads map and generates variables
 %param: mapfile ie: map.txt
 %output: map, in 2D array
@@ -13,17 +13,17 @@ fclose(fid);
 
 [i,j] = find(map==1);
 obst = [i j];
-[i,j] = find(map==-1);
-items = [i j];
+%[i,j] = find(map==-1);
+%items = [i j];
 
-[i,j] = find([map(1,1:end);zeros(size(map,1)-1,size(map,2))] == 2);
-gates = [i j];
-[i,j] = find([zeros(size(map,1),size(map,2)-1) map(1:end,end)] == 2);
-gates = [gates; i j];
-[i,j] = find([zeros(size(map,1)-1,size(map,2));map(end,1:end)] == 2);
-gates = [gates; i j];
-[i,j] = find([map(1:end,1) zeros(size(map,1),size(map,2)-1)] == 2);
-gates = [gates; i j];
+%[i,j] = find([map(1,1:end);zeros(size(map,1)-1,size(map,2))] == 2);
+%gates = [i j];
+%[i,j] = find([zeros(size(map,1),size(map,2)-1) map(1:end,end)] == 2);
+%gates = [gates; i j];
+%[i,j] = find([zeros(size(map,1)-1,size(map,2));map(end,1:end)] == 2);
+%gates = [gates; i j];
+%[i,j] = find([map(1:end,1) zeros(size(map,1),size(map,2)-1)] == 2);
+%gates = [gates; i j];
 
 
 end
